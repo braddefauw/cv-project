b4b937d46a86917429a7a65a3e4c32ce02ad5deb
Brad DeFauw <badefauw@gmail.com>
2022-08-08 09:03:13 -0500

    added education and experience components

---
+++ src/components/Education.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Education extends React.Component {
+    render() {
+      return (
+        <h1>Education info goes here</h1>
+      )
+    }
+}
+
+export default Education;
---
+++ src/components/Experience.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Experience extends React.Component {
+    render() {
+      return (
+        <h1>Experience goes here</h1>
+      )
+    }
+}
+
+export default Experience;
