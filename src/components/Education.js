52d6c3baf73ab7b805fe98c3be139e8001dc74f7
Brad DeFauw <badefauw@gmail.com>
2022-08-08 09:56:28 -0500

    experience component inputs added

--- src/components/Education.js
+++ src/components/Education.js
@@ -7,46 +7,46 @@ class Education extends React.Component {
           school: '',
           major: '',
           gradYear: '',
-        };
-    
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
-    
-      handleChange(event) {
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
-        
-        this.setState(
-          {[name]: value}
-          );
-      }
-    
-      handleSubmit(event) {
-        alert(`School: ${this.state.school}, Major: ${this.state.major}, Graduation: ${this.state.gradYear}`);
-        event.preventDefault();
-      }
-    
-      render() {
-        return (
-          <form onSubmit={this.handleSubmit}>
-            <label>
-              School:
-              <input name="school" type="text" value={this.state.name} onChange={this.handleChange} />
-            </label>
-            <label>
-              Major:
-              <input name="major" type="text" value={this.state.email} onChange={this.handleChange} />
-            </label>
-            <label>
-              Graduation Year:
-              <input name="gradYear" type="num" value={this.state.phone} onChange={this.handleChange} />
-            </label>
-            <input type="submit" value="Submit" />
-          </form>
-        )
-      }
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
     }
+
+    handleChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    
+    this.setState(
+        {[name]: value}
+        );
+    }
+
+    handleSubmit(event) {
+    alert(`School: ${this.state.school}, Major: ${this.state.major}, Graduation: ${this.state.gradYear}`);
+    event.preventDefault();
+    }
+
+    render() {
+    return (
+        <form onSubmit={this.handleSubmit}>
+        <label>
+            School:
+            <input name="school" type="text" value={this.state.name} onChange={this.handleChange} />
+        </label>
+        <label>
+            Major:
+            <input name="major" type="text" value={this.state.email} onChange={this.handleChange} />
+        </label>
+        <label>
+            Graduation Year:
+            <input name="gradYear" type="num" value={this.state.phone} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+        </form>
+    )
+    }
+}
 
 export default Education;
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,10 +1,60 @@
 import React from "react";
 
 class Experience extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          company: '',
+          position: '',
+          tasks: '',
+          start: '',
+          end: '',
+        };
+    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+    handleChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    
+    this.setState(
+        {[name]: value}
+        );
+    }
+
+    handleSubmit(event) {
+    alert(`Company: ${this.state.company}, Position: ${this.state.position}, Tasks: ${this.state.tasks}, Start Date: ${this.state.start}, End Date: ${this.state.end}`);
+    event.preventDefault();
+    }
+
     render() {
-      return (
-        <h1>Experience goes here</h1>
-      )
+    return (
+        <form onSubmit={this.handleSubmit}>
+        <label>
+            Company:
+            <input name="company" type="text" value={this.state.company} onChange={this.handleChange} />
+        </label>
+        <label>
+            Position:
+            <input name="position" type="text" value={this.state.email} onChange={this.handleChange} />
+        </label>
+        <label>
+            Tasks:
+            <input name="tasks" type="text" value={this.state.phone} onChange={this.handleChange} />
+        </label>
+        <label>
+            Start Date:
+            <input name="start" type="date" value={this.state.start} onChange={this.handleChange} />
+        </label><label>
+            End Date:
+            <input name="end" type="date" value={this.state.end} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+        </form>
+    )
     }
 }
 
