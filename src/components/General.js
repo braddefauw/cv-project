48b601e83cb26bb2a786d389ee46f27c232b777f
Brad DeFauw <badefauw@gmail.com>
2022-08-08 09:25:20 -0500

    general info inputs added

--- src/components/General.js
+++ src/components/General.js
@@ -1,11 +1,52 @@
 import React from "react";
 
 class General extends React.Component {
-    render() {
-      return (
-        <h1>General info goes here</h1>
-      )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      phone: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    
+    this.setState(
+      {[name]: value}
+      );
+  }
+
+  handleSubmit(event) {
+    alert(`Name: ${this.state.name}, Email: ${this.state.email}, Phone: ${this.state.phone}`);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input name="name" type="text" value={this.state.name} onChange={this.handleChange} />
+        </label>
+        <label>
+          Email
+          <input name="email" type="email" value={this.state.email} onChange={this.handleChange} />
+        </label>
+        <label>
+          Phone
+          <input name="phone" type="tel" value={this.state.phone} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    )
+  }
 }
 
 export default General;
